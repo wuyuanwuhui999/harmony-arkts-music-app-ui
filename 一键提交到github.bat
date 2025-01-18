ad4150d2a7d9f593da55354a9ccafef7dcdb113f
wuyuanwuhui99 <275018723@qq.com>
2025-01-18 19:42:14 +0800

    单独分离音乐项目

--- 一键提交到github.bat
+++ 一键提交到github.bat
@@ -1,4 +1,4 @@
 git remote rm origin
-git remote add origin https://github.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
+git remote add origin https://github.com/wuyuanwuhui99/Harmony-arkts-music-app-ui.git
 git push origin master
 pause
